b07dbfc58eafca002c44e0e8feb2a9c6a7a480a4
Ahmad AKBARY <ahmadakbary0908@gmail.com>
2026-07-18 13:56:10 +0430

    I added pie and time series charts with plotly express, and worked on layout, tabs, ccontainers and dividers

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-from charts.charts import households_by_province, households_by_gender, households_by_sector, households_by_survey_round, assistance_by_province
+from charts.charts import ( households_by_province,
+                            households_by_gender,
+                            households_by_sector,
+                            households_by_survey_round,
+                            assistance_by_province,
+                            assistance_by_sector,
+                            assistance_by_ip,
+                            average_satisfaction_by_province,
+                            average_hh_by_province,
+                            average_food_security_score_by_province,
+                            average_wash_access_score_by_province,
+                            assessments_by_month,
+                            total_assistance_by_month, avg_satisfaction_by_month
+
+                            )
 from utils.filters import apply_filters
 from utils.load_data import load_data
 from utils.calculations import (
@@ -17,7 +31,8 @@ from utils.calculations import (
 st.set_page_config(
     page_title="Humanitarian Monitoring Dashboard",
     page_icon="📊",
-    layout="wide"
+    layout="wide",
+    initial_sidebar_state="expanded"
 )
 
 # ----------------------------------------------------
@@ -42,32 +57,49 @@ average_satisfaction = get_average_satisfaction(filtered_df)
 # PAGE TITLE
 # ----------------------------------------------------
 
-st.title("📊 Humanitarian Monitoring Dashboard")
+st.title("🌍 Humanitarian Monitoring Dashboard")
+
+st.markdown(
+    """
+    This dashboard provides monitoring insights from humanitarian interventions,
+    including household demographics, assistance distribution,
+    beneficiary satisfaction, and outcome trends.
+    """
+)
+
 st.success("Dataset loaded successfully!")
 
 # ----------------------------------------------------
 # KPI CARDS
 # ----------------------------------------------------
+st.divider()
 
-col1, col2, col3, col4 = st.columns(4)
+with st.container():
 
-with col1:
-    st.metric("Households", total_households)
+    st.subheader("📌 Key Performance Indicators")
+    col1, col2, col3, col4 = st.columns(4)
 
-with col2:
-    st.metric("Provinces", total_provinces)
+    with col1:
+        st.metric("Households",
+                  f"{total_households:,}"
+        )
 
-with col3:
-    st.metric(
-        "Total Assistance",
-        f"${total_assistance:,.0f}"
-    )
+    with col2:
+        st.metric("Provinces",
+                  f"{total_provinces:,}"
+        )
 
-with col4:
-    st.metric(
-        "Avg Satisfaction",
-        f"{average_satisfaction:.1f}"
-    )
+    with col3:
+        st.metric(
+            "Total Assistance",
+            f"${total_assistance:,.0f}"
+        )
+
+    with col4:
+        st.metric(
+            "Avg Satisfaction",
+            f"{average_satisfaction:.1f}"
+        )
 
 # ----------------------------------------------------
 # DATA INFORMATION
@@ -76,59 +108,180 @@ with col4:
 st.write(f"Rows: {filtered_df.shape[0]}")
 st.write(f"Columns: {filtered_df.shape[1]}")
 
-# ----------------------------------------------------
-# Distribution Charts:
-# ----------------------------------------------------
 
-province_fig = households_by_province(filtered_df)
-gender_fig = households_by_gender(filtered_df)
-
-col1, col2 = st.columns(2)
-with col1:
-    st.plotly_chart(
-        province_fig,
-        width="stretch"
-    )
-
-with col2:
-    st.plotly_chart(
-        gender_fig,
-        width="stretch"
-    )
-
-sector_fig = households_by_sector(filtered_df)
-survey_round_fig = households_by_survey_round(filtered_df)
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.plotly_chart(
-        sector_fig,
-        width="stretch"
-    )
-
-with col2:
-    st.plotly_chart(
-        survey_round_fig,
-        width="stretch"
-    )
-
-
-# ----------------------------------------------------
-# Totals Charts:
-# ----------------------------------------------------
-
-assistance_province = assistance_by_province(filtered_df)
-st.plotly_chart(assistance_province, width="stretch")
-
-
-
-
-# ----------------------------------------------------
-# DATA TABLE
-# ----------------------------------------------------
-
-st.dataframe(
-    filtered_df,
-    width="stretch"
+tab_analysis, tab_trends, tab_data = st.tabs(
+    [
+        "📊 Analysis",
+        "⏳ Trends",
+        "📋 Data"
+    ]
 )
+
+
+with tab_analysis:
+
+    # ----------------------------------------------------
+    # Distribution Charts:
+    # ----------------------------------------------------
+    st.divider()
+    with st.container():
+        st.subheader("📊 Distribution Analysis")
+
+        province_fig = households_by_province(filtered_df)
+        gender_fig = households_by_gender(filtered_df)
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.plotly_chart(
+                province_fig,
+                width="stretch"
+            )
+
+        with col2:
+            st.plotly_chart(
+                gender_fig,
+                width="stretch"
+            )
+
+        sector_fig = households_by_sector(filtered_df)
+        survey_round_fig = households_by_survey_round(filtered_df)
+
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.plotly_chart(
+                sector_fig,
+                width="stretch"
+            )
+
+        with col2:
+            st.plotly_chart(
+                survey_round_fig,
+                width="stretch"
+            )
+
+
+    # ----------------------------------------------------
+    # Totals Charts:
+    # ----------------------------------------------------
+    st.divider()
+    with st.container():
+
+        st.subheader("💰 Assistance Analysis")
+
+        assistance_province = assistance_by_province(filtered_df)
+        assistance_ip = assistance_by_ip(filtered_df)
+        assistance_setor = assistance_by_sector(filtered_df)
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.plotly_chart(
+                assistance_province,
+                width="stretch"
+            )
+
+        with col2:
+            st.plotly_chart(
+                assistance_ip,
+                width="stretch"
+            )
+
+        with col3:
+            st.plotly_chart(
+                assistance_setor,
+                width="stretch"
+
+            )
+
+
+
+    # ----------------------------------------------------
+    # Average Charts:
+    # ----------------------------------------------------
+    st.divider()
+    with st.container():
+        st.subheader("📈 Average Indicators")
+
+        avg_satisfaction = average_satisfaction_by_province(filtered_df)
+        avg_hh = average_hh_by_province(filtered_df)
+        avg_wash = average_wash_access_score_by_province(filtered_df)
+        avg_food = average_food_security_score_by_province(filtered_df)
+
+
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.plotly_chart(
+                avg_satisfaction,
+                width="stretch"
+            )
+
+        with col2:
+            st.plotly_chart(
+                avg_hh,
+                width="stretch"
+            )
+
+        with col3:
+            st.plotly_chart(
+                avg_wash,
+                width="stretch"
+            )
+
+        with col4:
+            st.plotly_chart(
+                avg_food,
+                width="stretch"
+            )
+
+
+
+with tab_trends:
+    # ----------------------------------------------------
+    # Trend Charts:
+    # ----------------------------------------------------
+    st.divider()
+    with st.container():
+        st.subheader("⏳ Time Trends")
+
+        month_assess = assessments_by_month(filtered_df)
+        total_assess = total_assistance_by_month(filtered_df)
+        avg_satisfaction_month = avg_satisfaction_by_month(filtered_df)
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.plotly_chart(
+                month_assess,
+                width="stretch"
+
+            )
+
+
+        with col2:
+            st.plotly_chart(
+                total_assess,
+                width="stretch"
+            )
+
+
+        with col3:
+            st.plotly_chart(
+                avg_satisfaction_month,
+                width="stretch"
+
+            )
+
+    
+
+with tab_data:
+    # ----------------------------------------------------
+    # DATA TABLE
+    # ----------------------------------------------------
+    st.divider()
+    with st.container():
+        st.subheader("📋 Filtered Dataset")
+        st.dataframe(
+            filtered_df,
+            width="stretch"
+        )
--- charts/charts.py
+++ charts/charts.py
@@ -1,4 +1,5 @@
 import plotly.express as px
+import pandas as pd
 
 # ----------------------------------------------------
 # Distribution Charts:
@@ -98,3 +99,177 @@ def assistance_by_province(df):
     return fig
 
 
+
+def assistance_by_ip (df):
+    donor_count =(
+        df.groupby("implementing_partner")["assistance_value_usd"]
+        .sum()
+        .reset_index()
+    )
+
+    fig = px.pie (
+        donor_count,
+        names = "implementing_partner",
+        values= "assistance_value_usd",
+        title="Assistance by Implementing Partner",
+
+    )
+
+    return fig
+
+
+def assistance_by_sector (df):
+    sector_count =(
+        df.groupby("sector")["assistance_value_usd"]
+        .sum()
+        .reset_index()
+    )
+
+    fig = px.pie(
+        sector_count,
+        names = "sector",
+        values= "assistance_value_usd",
+        title="Assistance by Sector",
+    )
+
+    return fig
+
+
+# ----------------------------------------------------
+# Average Charts:
+# ----------------------------------------------------
+def average_satisfaction_by_province (df):
+    satisfaction_count = (
+        df.groupby("province")["satisfaction_level"]
+        .mean()
+        .reset_index()
+    )
+
+    fig = px.bar(
+        satisfaction_count,
+        x="province",
+        y="satisfaction_level",
+        title="Satisfaction by Province",
+    )
+
+    return fig
+
+def average_hh_by_province(df):
+    hh_count = (
+        df.groupby("province")["household_size"]
+        .mean()
+        .reset_index()
+    )
+
+    fig = px.bar(
+        hh_count,
+        x="province",
+        y="household_size",
+        title="Household by Province",
+    )
+
+    return fig
+
+def average_food_security_score_by_province (df):
+    security_count = (
+        df.groupby("province")["food_security_score"]
+        .mean()
+        .reset_index()
+    )
+
+    fig = px.bar(
+        security_count,
+        x="province",
+        y="food_security_score",
+        title="Food Security by Province",
+    )
+
+    return fig
+
+def average_wash_access_score_by_province (df):
+    wash_score_count = (
+        df.groupby("province")["wash_access_score"]
+        .mean()
+        .reset_index()
+
+    )
+
+    fig = px.bar(
+        wash_score_count,
+        x="province",
+        y="wash_access_score",
+        title="Wash Access by Province",
+
+    )
+
+    return fig
+
+# ----------------------------------------------------
+# Trends Charts:
+# ----------------------------------------------------
+
+def assessments_by_month(df):
+
+    df["assessment_date"] = pd.to_datetime(df["assessment_date"])
+
+    df["month"] = df["assessment_date"].dt.to_period("M").astype(str)
+
+    monthly_assessments = (
+        df.groupby("month")
+          .size()
+          .reset_index(name="assessments")
+    )
+
+    fig = px.line(
+        monthly_assessments,
+        x="month",
+        y="assessments",
+        title="Household Assessments by Month",
+        markers=True
+    )
+
+    return fig
+
+
+def total_assistance_by_month(df):
+    df["assessment_date"] = pd.to_datetime(df["assessment_date"])
+    df["month"] = df["assessment_date"].dt.to_period("M").astype(str)
+
+    total_monthly_assessments_value = (
+        df.groupby("month")["assistance_value_usd"]
+        .sum()
+        .reset_index()
+    )
+
+    fig = px.line(
+        total_monthly_assessments_value,
+        x="month",
+        y="assistance_value_usd",
+        title="Total Assistance by Month",
+        markers=True
+    )
+
+    return fig
+
+
+def avg_satisfaction_by_month(df):
+    df["assessment_date"] = pd.to_datetime(df["assessment_date"])
+    df["month"] = df["assessment_date"].dt.to_period("M").astype(str)
+
+    satisfaction_by_month = (
+        df.groupby("month")["satisfaction_level"]
+        .mean()
+        .reset_index()
+
+    )
+
+    fig  = px.line (
+        satisfaction_by_month,
+        x="month",
+        y="satisfaction_level",
+        title="Satisfaction by Month",
+        markers=True
+
+    )
+
+    return fig
